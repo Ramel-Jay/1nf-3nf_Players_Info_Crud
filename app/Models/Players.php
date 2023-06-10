58cc999ecd5f25da909d5f82786770c687da13d4
Ramel Jay <rameljey15@gmail.com>
2023-06-10 21:21:45 +0800

    created the model

--- app/Models/Players.php
+++ app/Models/Players.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'role_id',
+        'game_id',
         'name',
-        'email',
-        'password',
+        'ign',
+        'address',
+        'state_code'
     ];
 
     /**
