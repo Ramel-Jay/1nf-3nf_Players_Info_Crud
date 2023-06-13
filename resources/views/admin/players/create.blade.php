8a46d51e3f1b779a592bcedcc6673538f2a83570
Ramel Jay <rameljey15@gmail.com>
2023-06-13 22:02:01 +0800

    jkasdjf

--- resources/views/admin/players/create.blade.php
+++ resources/views/admin/players/create.blade.php
@@ -39,7 +39,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     </head>
-    <body class="antialiased">
+    <body class="antialiased bg-gray-700">
         <section class="bg-white dark:bg-gray-900">
             <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
                 <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Register Player</h2>
--- resources/views/admin/players/index.blade.php
+++ resources/views/admin/players/index.blade.php
@@ -14,7 +14,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     </head>
-    <body class="antialiased">
+    <body class="antialiased bg-gray-700">
         {{-- <div>
             <a href={{ url('admin/players/create') }} class="text-white text-uppercase bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Player</a>
         </div> --}}
